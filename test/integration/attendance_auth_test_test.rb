4eea2c6ccdeed577a6249451c3bc6d4c444723a4
serdg0 <sergio.diazg92@gmail.com>
2019-10-10 16:38:17 -0500

    added more tests

---
+++ test/integration/attendance_auth_test_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class AttendanceAuthTestTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = User.new(name: 'Example', email: 'example@user.com')
+    @user2 = User.new(name: 'Example1', email: 'example1@user.com')
+    @event = @user.events.build(title: 'Example', content: 'Whatever',
+                                start: 'Mon, 30 Sep 2019 22:53:44 UTC +00:00')
+  end
+
+  test 'only invited users can be attendees' do
+    assert_not @event.attendees.include?(@user2)
+  end
+
+  test 'invite user to become attendee' do
+    @event.attendees << @user2
+    assert @event.attendees.include?(@user2)
+  end
+
+end
--- test/models/attendance_test.rb
+++ test/models/attendance_test.rb
@@ -3,7 +3,5 @@
 require 'test_helper'
 
 class AttendanceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
 end
